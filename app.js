712623005ac0eacac1c6107ff8770e8a28742e71
Anas Shaikh <shaikhanas246@gmail.com>
2021-07-13 11:22:14 +0530

    updated files

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const address = process.argv[2];
-geocode(address, (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
 
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+  console.log("please provide an address");
+} else {
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
     if (error) {
-      return console.log("Error", error);
+      return console.log(error);
     }
-    console.log(data.location);
-    console.log(forecastData);
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log("Error", error);
+      }
+      console.log(location);
+      console.log(forecastData);
+    });
   });
-});
+}
