c75a9acab42e6f64e8e10088af44c21d098f659a
Anas Shaikh <shaikhanas246@gmail.com>
2021-07-13 11:19:26 +0530

    destructured objects

--- utils/geocode.js
+++ utils/geocode.js
@@ -6,16 +6,16 @@ const geocode = (address, callback) => {
     encodeURIComponent(address) +
     ".json?access_token=pk.eyJ1IjoiYW5hczk2OTkiLCJhIjoiY2txeXRicTQ0MDl6cTMwbW40bHQzMDFqeiJ9.1z_usGG57eP_PSHAAC5JjA";
 
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("unable to connect to geocode service", undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("unable to find the location.Try another search", undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0]["center"][1],
-        longitude: response.body.features[0]["center"][0],
-        location: response.body.features[0].place_name,
+        latitude: body.features[0]["center"][1],
+        longitude: body.features[0]["center"][0],
+        location: body.features[0].place_name,
       });
     }
   });
